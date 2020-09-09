9b348f24ed734e785cc078a1d891e3b33e0655d2
jon mikel <jonnymikel8@gmail.com>
2020-09-09 12:11:45 +0200

    Primera version

---
+++ src/main/java/com/artistas/api/ZarautzPunturaApplication.java
@@ -0,0 +1,13 @@
+package com.artistas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarautzPunturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarautzPunturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artistas/api/ZarautzPunturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarautzPunturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
